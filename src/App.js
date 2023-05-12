3b28472cd69e60b45e49116e451532cdb0558226
Mudit Mathur <muditmathur097@gmaile.com>
2023-05-12 18:04:28 +0530

    adding apps source code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello This Is DevOps Farm Page. </h1>
+      
+    </div>
+  );
+}
+
+export default App;
